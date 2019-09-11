0926d6cd1af08b37aea48790f4cca33c2593c5e2
enrique.mena <ee.mena7@gmail.com>
2019-09-11 15:57:55 -0600

    Adaptador personalizado [Completo]

--- app/src/main/java/com/example/adaptador/MainActivity.java
+++ app/src/main/java/com/example/adaptador/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.adaptador;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.adaptador.adapter.DiaHorarioAdapter;
 import com.example.adaptador.modelo.DiaHorario;
@@ -15,13 +19,14 @@ public class MainActivity extends AppCompatActivity {
 
     private ListView horario;
     private ArrayList<DiaHorario> lista;
+    private TextView opcion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] titulos = getResources().getStringArray(R.array.horario_de_clases);
+        final String[] titulos = getResources().getStringArray(R.array.horario_de_clases);
         String[] subtitulos = getResources().getStringArray(R.array.dias_semana);
         //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,titulos);
         //ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.horario_de_clases,android.R.layout.simple_list_item_1);
@@ -33,7 +38,19 @@ public class MainActivity extends AppCompatActivity {
 
         DiaHorarioAdapter adapter = new DiaHorarioAdapter(this,lista);
 
+        opcion = findViewById(R.id.opcion);
         horario = findViewById(R.id.lista);
         horario.setAdapter(adapter);
+
+        horario.setOnItemClickListener(new OnItemClickListener() {
+                                           @Override
+                                           public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                                               opcion.setText("Opcion: " + titulos[i]);
+                                           }
+                                       }
+
+        );{
+
+        }
     }
 }
